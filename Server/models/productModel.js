4537d6644996168886a6b530b3213971ac0fa80d
Al-Abir <sheikhabir196@gmail.com>
2025-03-13 00:01:30 +0600

    Product Model

---
+++ Server/models/productModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true
+     },
+     slug:{
+        type:String,
+        lowercase:true
+     },
+     description:{
+        type: String,
+        required:true
+     },
+     price:{
+        type:Number,
+        required:true
+     },
+     category:{
+        type:mongoose.ObjectId,
+        ref:"Category",
+        required:true
+     },
+     quantity:{
+         type:Number,
+         required:true
+     },
+     photo:{
+        type:Buffer,
+        contentType:String
+     },
+     shipping:{
+        type:Boolean
+     }
+     
+},{timestamps:true})
+
+const Product = mongoose.model("products",productSchema)
+
+module.exports = Product;
