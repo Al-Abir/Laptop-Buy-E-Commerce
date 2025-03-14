18c1caa52fd189399818c8a6bb721e71a856e73a
Al-Abir <sheikhabir196@gmail.com>
2025-03-14 17:51:45 +0600

    Client Table

--- Client/src/pages/Admin/CreateCategory.jsx
+++ Client/src/pages/Admin/CreateCategory.jsx
@@ -1,45 +1,72 @@
+import { useEffect, useState } from "react";
+import Layout from "../../components/Layout/Layout";
+import AdminMenu from "../../components/Layout/AdminMenu";
+import toast from "react-hot-toast";
+import axios from "axios";
 
-import { useEffect,useState } from 'react'
-import Layout from '../../components/Layout/Layout'
-import AdminMenu from '../../components/Layout/AdminMenu'
-import toast from 'react-hot-toast'
-import axios from 'axios'
 const CreateCategory = () => {
-  const[categories,setCategorie] = useState([]);
+  const [categories, setCategories] = useState([]);
 
- // get all data
- const getAllCategory = async()=>{
-  
-     try {
-          const{data} = await axios.get(`${import.meta.env.VITE_API_URL}/api/v1/category/category-all`)
-          if(data.success){
-            setCategorie(data.categories)
-          }
-     } catch (error) {
-          console.log(error)
-          toast.error("Something went wrong in getting category")
+  // Fetch categories
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get(
+        `${import.meta.env.VITE_API_URL}/api/v1/category/category-all`
+      );
+      if (data.success) {
+        setCategories(data.categories);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong in getting category");
+    }
+  };
 
-     }
- }
- useEffect(()=>{
-      getAllCategory()
- },[])
+  useEffect(() => {
+    getAllCategory();
+  }, []);
+
   return (
     <Layout>
-         <div className="container mx-auto p-4">
+      <div className="container mx-auto p-4">
         <div className="flex gap-4">
           {/* Sidebar */}
           <div className="w-1/4 mt-3 p-4">
             <AdminMenu />
           </div>
           {/* Main Content */}
-          <div className="w-3/4 mt-3 ">
-               <h2>Create Category</h2>
+          <div className="w-3/4 mt-3">
+            <h2>Create Category</h2>
+            <div className="overflow-x-auto w-full">
+              <table className="w-1/2 border-collapse border border-gray-300">
+                <thead>
+                  <tr className="border-b border-gray-300 bg-gray-100">
+                    <th className="text-start px-4 py-2">Name</th>
+                    <th className="text-start px-4 py-2">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {categories.map((c) => (
+                    <tr key={c._id} className="border-b border-gray-200">
+                      <td className="px-4 py-2">{c.name}</td>
+                      <td className="px-4 py-2 flex gap-5">
+                        <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
+                          Edit
+                        </button>
+                        <button className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600">
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default CreateCategory
+export default CreateCategory;
